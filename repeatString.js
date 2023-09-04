330643aa09baa46eeaef5cd462d22298f110e2f0
Shruti Bajpai <bajpaishruti8467@gmail.com>
2023-09-05 01:38:20 +0530

    Make some changes

---
+++ repeatString.js
@@ -0,0 +1,19 @@
+function repeat(str, num){
+    var output ="";
+    if (num>=0){
+        for (var i=0; i<num; i++){
+            output = output + str;
+        }
+        console.log(output);
+    }
+    else{
+        console.log("ERROR");
+    }
+}
+
+repeat("shruti", 4);
+repeat("pooja", "1");
+repeat("tanu", 0);
+repeat("manu", -2);
+
+
